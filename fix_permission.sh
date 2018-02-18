613ed23146ed2766f57aa40a21ca593b583b7bc7
Alex Drlica-Wagner <kadrlica@fnal.gov>
2018-02-18 12:32:40 -0800

    Updated permissions for material

--- fix_permission.sh
+++ fix_permission.sh
@@ -10,7 +10,7 @@ set -e
 chmod o+rx .
 chmod o+r .htaccess *.html *.shtml *.php.inc
 chmod o+rx *.php
-chmod -R o+rX assets exclusive
+chmod -R o+rX assets exclusive material
 chmod -R o+rwX .db
 
 # end subshell
