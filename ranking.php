2a03af30b673db39b5869012160018cdfa5900fd
Alex Drlica-Wagner <kadrlica@fnal.gov>
2018-12-31 12:17:50 -0800

    get ranking without users database

--- ranking.php
+++ ranking.php
@@ -2,18 +2,16 @@
 
 include "common.php.inc";
 $dbh = getDBHandle();
-
-$query = 'SELECT userid, total_files, flagged_files FROM submissions WHERE release=? AND total_files > 0 ORDER BY total_files DESC';
+ 
+$query = 'SELECT userid, count(userid) as total_files, sum(problem > 0) as flagged_files FROM qa GROUP BY userid ORDER BY total_files DESC';
 if (isset($_GET['limit'])) {
   if (is_numeric($_GET['limit'])) {
     $query .= ' LIMIT ?';
     $stmt = $dbh->prepare($query);
-    $stmt->bindParam(1, $config['release'], PDO::PARAM_STR, 4);
-    $stmt->bindParam(2, $_GET['limit'], PDO::PARAM_INT);
+    $stmt->bindParam(1, $_GET['limit'], PDO::PARAM_INT);
   }
 } else {
   $stmt = $dbh->prepare($query);
-  $stmt->bindParam(1, $config['release'], PDO::PARAM_STR, 4);
 }
 $stmt->execute();
 check_or_abort($stmt);
