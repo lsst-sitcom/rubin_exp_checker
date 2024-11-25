d67aaff7a762691471a873d3b4814882e8a37c4a
Alex Drlica-Wagner <kadrlica@fnal.gov>
2024-11-24 22:22:01 -0800

    adding butler and s3 image access

--- sql/files.sql
+++ sql/files.sql
@@ -9,6 +9,7 @@ CREATE TABLE files (
 );
 CREATE INDEX files_ccd_idx ON "files" (ccd);
 CREATE INDEX files_expname_idx ON "files" (expname);
+CREATE UNIQUE INDEX files_expname_ccd_index ON "files" (expname, ccd);
 
 CREATE TABLE qa (
  qaid INTEGER PRIMARY KEY ASC,
@@ -23,3 +24,10 @@ CREATE TABLE qa (
 CREATE INDEX qa_fileid_idx ON "qa" (fileid);
 CREATE INDEX qa_problem_idx ON "qa" (problem);
 CREATE INDEX qa_userid_idx ON "qa" (userid);
+
+CREATE TABLE submissions (
+ userid INTEGER NO NULL,
+ total_files INT NOT NULL DEFAULT 0,
+ flagged_files INT NOT NULL DEFAULT 0
+);
+CREATE INDEX submission_userid_idx ON submissions(userid);
